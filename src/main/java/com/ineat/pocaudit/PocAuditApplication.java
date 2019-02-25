1727f2d51f80852c7fdd0b65ebdafc6609d3aa7b
Mathias Deremer <mathias.deremer.partner@decathlon.com>
2019-02-25 11:39:17 +0100

    Init repository

---
+++ src/main/java/com/ineat/pocaudit/PocAuditApplication.java
@@ -0,0 +1,13 @@
+package com.ineat.pocaudit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocAuditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocAuditApplication.class, args);
+	}
+
+}
