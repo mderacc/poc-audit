8698ddc377f6ad29bd731edaba30200b6451aec1
uendelig <mathias.deracc@gmail.com>
2019-02-25 13:54:35 +0100

    Add metrics

--- src/main/java/com/ineat/pocaudit/MainController.java
+++ src/main/java/com/ineat/pocaudit/MainController.java
@@ -4,6 +4,9 @@ import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
+import com.ineat.pocaudit.data.Movie;
+import com.ineat.pocaudit.data.MovieRepository;
+import io.micrometer.core.instrument.Metrics;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,13 +19,16 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
-public class MovieController {
+public class MainController {
 
 	@Autowired
 	private MovieRepository movieRepository;
 
 	@GetMapping("/movies")
 	public List<Movie> retrieveAllMovies() {
+
+		String result = "success"; // or something else
+		Metrics.counter("message.sent", "result", result).increment();
 		return movieRepository.findAll();
 	}
 }
--- src/main/java/com/ineat/pocaudit/data/MovieRepository.java
+++ src/main/java/com/ineat/pocaudit/data/MovieRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MovieRepository extends JpaRepository<Student, Long>{
+public interface MovieRepository extends JpaRepository<Movie, Long>{
 
 }
